8666654f0378df5acd3829284f37e2b5bbe846b5
jbees918 <josh.beestoned@gmail.com>
2024-02-07 17:56:03 -0600

    contr

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // This is a reference to the Rigidbody component
+    private Rigidbody rb;
 
-    public Rigidbody
-
-    public float fwdForce = 2000f;
-
-
-    public float speed = 7f;
+    public float speed = 5f; // Variable that determines the movement speed
 
     void Start()
     {
-        // init
+        // Get the Rigidbody component attached to the same GameObject
+        rb = GetComponent<Rigidbody>();
     }
-    
-    void Update()
+
+    void FixedUpdate()
     {
-        rb.AddForce(0, 0, fwdForce * Time.deltaTime);
+        // Capture input for horizontal and vertical movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calculate movement vector
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Apply movement to the Rigidbody component's velocity
+        rb.velocity = movement * speed;
     }
 }
