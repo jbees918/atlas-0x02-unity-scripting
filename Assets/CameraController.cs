10ec61fe09bb3186cb0a8dc0f84b883178954f9b
jbees918 <josh.beestoned@gmail.com>
2024-02-08 01:44:40 -0600

    coinage affairs

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Reference to the player object
+    // Reference to player object
     public GameObject player;
 
     // Offset from player position
     public Vector3 offset = new Vector3(0,  2, -5);
 
-
-    // Update is called once per frame
     void Update()
     {
          // Set the position of the camera to player position plus soffset
