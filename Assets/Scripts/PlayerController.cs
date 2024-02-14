74083b8b19115a8e34bf13c448d9069666933bb0
jbees918 <josh.beestoned@gmail.com>
2024-02-14 05:59:03 -0600

    aded using directive

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
